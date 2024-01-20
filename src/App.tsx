9f57bec0e8146f582a16abfb39a65cfddf0584e3
Nathan MacDonald <natemacd97@gmail.com>
2024-01-19 20:45:59 -0400

    got scroll snap working

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
+//TODO: dark mode
+
+import { ReactNode } from "react";
+
+const SnapSection = ({ children }: { children: ReactNode }) => {
+  return (
+    <section className="snap-center flex items-center justify-center h-[100vh] ">
+      {children}
+    </section>
+  );
+};
+
 const App = () => {
   return (
-    <>
-      <h1>dringus</h1>
-    </>
+    <div className=" w-100 snap-mandatory snap-y h-[100vh] overflow-y-scroll">
+      <SnapSection>
+        <h1 className="text-7xl">Hi</h1>
+      </SnapSection>
+      <SnapSection>
+        <h1 className="text-7xl">Hi</h1>
+      </SnapSection>
+      <SnapSection>
+        <h1 className="text-7xl">Hi</h1>
+      </SnapSection>
+    </div>
   );
 };
 
