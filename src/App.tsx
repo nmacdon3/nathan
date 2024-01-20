1b60f6db81693328ed964f68f8d9dbeeb9eb9505
Nathan MacDonald <natemacd97@gmail.com>
2024-01-19 21:28:04 -0400

    got a typewriter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 //TODO: dark mode
 
 import { ReactNode } from "react";
+import Typewriter from "typewriter-effect";
 
 const SnapSection = ({ children }: { children: ReactNode }) => {
   return (
@@ -12,12 +13,33 @@ const SnapSection = ({ children }: { children: ReactNode }) => {
 
 const App = () => {
   return (
-    <div className=" w-100 snap-mandatory snap-y h-[100vh] overflow-y-scroll">
+    <div className="text-zinc-800 w-100 snap-mandatory snap-y h-[100vh] overflow-y-scroll font-roboto">
       <SnapSection>
-        <h1 className="text-7xl">Hi</h1>
+        <h1 className="text-8xl flex ">
+          {/* <Typewriter
+            options={{
+              strings: ["Hi"],
+              autoStart: true,
+              deleteSpeed: 1000000,
+              loop: true,
+            }}
+          />{" "} */}
+          Hi
+          <div className="ms-8 animate-wiggle ">&#x1F44B;</div>
+        </h1>
       </SnapSection>
       <SnapSection>
-        <h1 className="text-7xl">Hi</h1>
+        <h1 className="text-5xl flex ">
+          <Typewriter
+            options={{
+              strings: ["This is my website"],
+              deleteSpeed: 1000000,
+              autoStart: true,
+              loop: true,
+              cursor: "_",
+            }}
+          />
+        </h1>
       </SnapSection>
       <SnapSection>
         <h1 className="text-7xl">Hi</h1>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+      },
+      keyframes: {
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-3deg)" },
+          "50%": { transform: "rotate(3deg)" },
+        },
+      },
+      animation: {
+        wiggle: "wiggle 1s ease-in-out infinite",
+      },
+    },
   },
   plugins: [],
-}
-
+};
